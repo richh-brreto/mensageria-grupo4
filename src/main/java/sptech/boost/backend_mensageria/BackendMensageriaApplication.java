008a1b8c1215be58cf55ed5332f8b7849fa5a24a
Rafael Rodrigues <rafael.arodrigues@sptech.school>
2026-08-14 19:10:50 -0300

    feat:Project structure

---
+++ src/main/java/sptech/boost/backend_mensageria/BackendMensageriaApplication.java
@@ -0,0 +1,13 @@
+package sptech.boost.backend_mensageria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMensageriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMensageriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sptech/boost/backend_mensageria/BackendMensageriaApplicationTests.java
@@ -0,0 +1,13 @@
+package sptech.boost.backend_mensageria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMensageriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
